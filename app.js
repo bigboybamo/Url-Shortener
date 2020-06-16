255f39e596b8c0aa0d63bd2edfc44ea7e019be98
Bamiji Oyetubo <oyetubobamiji@gamil.com>
2020-06-16 13:38:48 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded());
+const urlShortener = require('node-url-shortener');
+const port = 3001;
+
+app.get('/',(req,res)=>{
+    res.sendfile('url.html',{root: __dirname})
+});
+
+app.post('/url',(req,res)=>{
+    const url = req.body.url;
+    urlShortener.short(url,(err, shortUrl)=>{
+        res.send(shortUrl);
+    })
+    })
+
+app.listen(port,()=>{
+    console.log(`Url-shortener listening on ${port}`);
+
+});
+
+
+
